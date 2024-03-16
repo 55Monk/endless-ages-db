d11a01fb0ee3e27f047cd2e71c7e7f527d05f2dd
55 Monk <55Monk@proton.me>
2024-03-16 00:20:25 -0500

    update discord ID

--- src/components/WebsiteHeader/WebsiteHeader.tsx
+++ src/components/WebsiteHeader/WebsiteHeader.tsx
@@ -15,7 +15,7 @@ export default function WebsiteHeader() {
           <img src={discordLogo} alt="EA Server Discord" className="h-6 w-6" />
         </a>
         <span className="absolute left-1/2 -translate-x-1/2 translate-y-3/4 rounded bg-neutral-800 px-2 py-1 text-center text-sm text-neutral-100 opacity-0 transition-opacity group-hover:opacity-100">
-          Need&nbsp;help? 55&nbsp;Monk#4855
+          Need&nbsp;help? 55monk
         </span>
       </div>
       <a href="https://github.com/55Monk/endless-ages-db">
